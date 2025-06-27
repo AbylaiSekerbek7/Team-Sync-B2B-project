bbbea712456a7b7059c9bd8c19f779bce2fe9815
Abylai <221714@astanait.edu.kz>
2025-06-27 15:43:37 +0200

    Created Http Status codes updated Error Handler usage

--- backend/src/middlewares/errorHandler.middleware.ts
+++ backend/src/middlewares/errorHandler.middleware.ts
@@ -1,14 +1,15 @@
 import { ErrorRequestHandler } from "express";
+import { HTTPSTATUS } from "../config/http.config";
 
 export const errorHandler: ErrorRequestHandler = (error, req, res, next): any => {
     console.error(`Error Occured on Path: ${req.path}`, error);
     if (error instanceof SyntaxError) {
-        return res.status(400).json({
+        return res.status(HTTPSTATUS.BAD_REQUEST).json({
             message: "Invalid JSON format. Please check your Request body",
         });
     };
 
-    return res.status(500).json({
+    return res.status(HTTPSTATUS.INTERNAL_SERVER_ERROR).json({
         message: "Internal server error",
         error: error?.message || "Uknown error occured" // if the real error exist we will display it, either "Unknown error occured"
     });
